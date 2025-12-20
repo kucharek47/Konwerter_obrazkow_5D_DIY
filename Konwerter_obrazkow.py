4e22df0916d21739382064c3df672f00e7703cab
Kucharek47 <matzar0@wp.pl>
2025-12-20 14:38:27 +0100

    0.0.4

--- Konwerter_obrazkow.py
+++ Konwerter_obrazkow.py
@@ -2,44 +2,75 @@ import json
 import math
 import numpy as np
 import cv2
-
+import string
+import random
 
 class Konwerter:
-    def __init__(self, open_img_cv2, szerokosc_mm, wysokosci_mm, wielkosc_pola_mm=3):
+    def __init__(self, open_img_cv2, szerokosc_mm, wysokosci_mm, wielkosc_pola_mm=3, path_zapisu=False):
         self.open_img_cv2 = open_img_cv2
         self.szerokosc_mm = szerokosc_mm
         self.wysokosci_mm = wysokosci_mm
         self.wielkosc_pola_mm = wielkosc_pola_mm
         self.slownik_posiadanych_kolorow = json.load(open("slownik_posiadanych_kolorow.json",encoding="utf-8"))
-
-        self.main()
+        self.potrzebne = {}
+        self.path_zapisu = path_zapisu or "static/wygenerowane/" + "".join(random.choices(string.ascii_uppercase + string.ascii_lowercase + string.digits, k=30)) + ".png"
+        try:
+            self.status_dzialania = True
+            self.main()
+            self.status_dzialania = False
+        except Exception as e:
+            print(e)
     def main(self):
         h_px, w_px, _ = self.open_img_cv2.shape
         h_pola, w_pola, do_szerkosci = self.przeliczenie_px_na_pola(h_px,w_px)
 
-        dopasowny_obrazek_cv2 = cv2.resize(self.open_img_cv2, (h_pola,w_pola), interpolation=cv2.INTER_AREA)
+        dopasowny_obrazek_cv2 = cv2.resize(self.open_img_cv2, (w_pola,h_pola), interpolation=cv2.INTER_AREA)
         rgb_cv2 = cv2.cvtColor(dopasowny_obrazek_cv2, cv2.COLOR_BGR2RGB)
         macierz_obrazka = self.konwersja_kolorow_macierzy(rgb_cv2.tolist())
 
         self.rysowanie(macierz_obrazka)
-    def rysowanie(self, macierz, szerokosc_mm, wysokosci_mm, wielkosc_pola_mm):
-        szerokosc_px = int(szerokosc_mm * 11,81)
-        wysokosci_px = int(wysokosci_mm * 11,81)
-        wielkosc_pola_mm = int(wielkosc_pola_mm * 11,81)
+    def path(self):
+        return self.path_zapisu
+    def potrzebne_masz(self):
+        potrzebne_masz = {}
+        for x in self.potrzebne:
+            potrzebne_masz[x] = [self.potrzebne[x], self.slownik_posiadanych_kolorow[x]["ilosc"]]
+        return potrzebne_masz
+    def rysowanie(self, macierz):
+        szerokosc_px = int(self.szerokosc_mm * 11.81)
+        wysokosci_px = int(self.wysokosci_mm * 11.81)
+        wielkosc_pola_px = int(self.wielkosc_pola_mm * 11.81)
         plotno = np.ones((wysokosci_px, szerokosc_px, 3), dtype=np.uint8) * 255
         x_old = 0
         y_old = 0
         liczenie_x = 0
         liczenie_y = 0
-        for x in range(0,wysokosci_px,wielkosc_pola_mm):
-            for y in range(0,szerokosc_px,wielkosc_pola_mm):
-                cv2.rectangle(plotno, (x_old,y_old),(x,y), (macierz[liczenie_x][liczenie_y]["rgb"][2], macierz[liczenie_x][liczenie_y]["rgb"][1], macierz[liczenie_x][liczenie_y]["rgb"][0]),-1)
-                znak = macierz[liczenie_x][liczenie_y]["oznaczenie"]
-                #TODO dodaj przesuniecie znaku i naloz go putText
+        for x in range(0,szerokosc_px,wielkosc_pola_px):
+            if len(macierz[0]) <= liczenie_x:
+                break
+            for y in range(0,wysokosci_px,wielkosc_pola_px):
+                if len(macierz) <= liczenie_y:
+                    break
+                cv2.rectangle(plotno, (x_old, y_old), (x, y),
+                              (macierz[liczenie_y][liczenie_x]["rgb"][2], macierz[liczenie_y][liczenie_x]["rgb"][1],
+                               macierz[liczenie_y][liczenie_x]["rgb"][0]), -1)
+                znak = macierz[liczenie_y][liczenie_x]["oznaczenie"]
+                cv2.putText(
+                    plotno,
+                    znak,
+                    (x_old, y - int(wielkosc_pola_px/ 5)),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    0.7,
+                    (0, 0, 0) if macierz[liczenie_y][liczenie_x]["rgb"][2] + macierz[liczenie_y][liczenie_x]["rgb"][1] + macierz[liczenie_y][liczenie_x]["rgb"][0] > 380 else (255, 255, 255),
+                    1,
+                    cv2.LINE_AA
+                )
                 y_old = y
                 liczenie_y += 1
+            liczenie_y = 0
             x_old = x
             liczenie_x += 1
+        cv2.imwrite(self.path_zapisu, plotno)
 
     def konwersja_kolorow_macierzy(self,macierz):
         macierz_pola = []
@@ -55,7 +86,12 @@ class Konwerter:
                     "nazwa":najblizszy_kolor,
                     "oznaczenie":self.slownik_posiadanych_kolorow[najblizszy_kolor]["oznaczenie"]
                 })
+                try:
+                    self.potrzebne[najblizszy_kolor] += 1
+                except:
+                    self.potrzebne[najblizszy_kolor] = 1
             macierz_pola.append(lista_przedu)
+        return macierz_pola
 
     def przeliczenie_px_na_pola(self, h_px, w_px):
         skala_kartki = self.wysokosci_mm/self.szerokosc_mm
@@ -75,4 +111,4 @@ class Konwerter:
             do_szerokosci = True
         return wysokosc, szerokosc, do_szerokosci
 if __name__ == '__main__':
-    konwerter = Konwerter(cv2.imread("zebra.webp"), 419,600)
+    konwerter = Konwerter(cv2.imread("zebra.jpg"), 210,297)
--- app.py
+++ app.py
@@ -1,12 +1,45 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import cv2
+import numpy as np
+import Konwerter_obrazkow
 
+slownik_kartki = {
+    "A0": [841,1189],
+    "A1": [594,841],
+    "A2": [420,594],
+    "A3": [297,420],
+    "A4": [210,297],
+    "A5": [148,219],
+}
 app = Flask(__name__)
-
+CORS(app, resources={r"/api/*": {"origins": "http://localhost:4200"}})
 
 @app.route('/')
-def hello_world():  # put application's code here
+def hello_world():
     return 'Hello World!'
+#note: API
+@app.route('/api/zmiana_obrazka', methods=["POST"])
+def api_zmiana_obrazka():
+    if 'image' not in request.files:
+        return jsonify({'error': 'No file part'}), 400
 
+    size_name = request.form.get('rozmiar_kartki', 'A4')
+
+    file = request.files['image']
+    file_bytes = np.frombuffer(file.read(), np.uint8)
+    cv_image = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
+
+    if cv_image is None:
+        return jsonify({'error': 'Failed to decode image'}), 400
+    try:
+        w, h = slownik_kartki[size_name[:2]] if len(size_name) == 3 and size_name[2] == "R" else slownik_kartki[size_name[:2]][::-1]
+        konwerter = Konwerter_obrazkow.Konwerter(cv_image, w, h)
+        print(konwerter.potrzebne_masz())
+        return jsonify({'message': 'Plik przetworzony pomyślnie', 'lok': konwerter.path(), "potrzebne":konwerter.potrzebne_masz()})
+    except Exception as e:
+        print(e)
+        return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=42310, debug=True)
